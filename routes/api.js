1510f7a8d0bfb577b5700b76d965da1cb504cb56
Hannyajin <talmo.christian@gmail.com>
2014-10-13 10:28:45 +0300

    fixed ajax uploads and added link stacking

--- routes/api.js
+++ routes/api.js
@@ -13,17 +13,80 @@ var limit = 100;
 
 /* POST */
 router.post('/upload', function(req, res) {
-  var json = JSON.parse(req.body);
-
+  var json = JSON.stringify(req.body);
   console.log(json);
-  res(200);
-});
-router.post('/get', function(req, res) {
-  var json = JSON.parse(req.body);
 
-  console.log(json);
-  res(200);
+  // test data
+  var data = {
+    imgsrc: "http://i.imgur.com/aKndIcZ.jpg",
+    title: "Some Title",
+    desc: "Lorem Ipsum..."
+  }
+
+  // fetch the url data
+
+  json = JSON.parse(json);
+
+  if (json.url) {
+
+      getImgInfo(json.url, function(err, info) {
+        if (err) {
+          console.log(err);
+          res.status(500);
+          res.end();
+          return;
+        }
+
+        var msg = {
+          title: info.title || "No Title Found.",
+          desc: info.desc || "No Description Found.",
+          imgsrc: "/api/thumbnail/" + parseuri(json.url).host
+        }
+
+        res.send(msg)
+        res.end();
+      });
+
+  } else {
+    res.status(400).end();
+  }
+
 });
+
+router.get('/upload', function(req, res) {
+});
+
+function getImgData(url, callback) {
+  if (!cache[url] || !cache[url].data) {
+    // cache link
+    cache[url] = {
+      url: url,
+      hits: 0,
+      created: new Date().getTime()
+    }
+
+    jthumb.getSiteThumbnail(url, function(err, data) {
+      if (err) {
+        console.log(err);
+        callback(err);
+        return;
+      }
+
+      // cache it
+      var obj = cache[url];
+      obj.data = data;
+      obj.hits++;
+
+      count++;
+
+      callback(null, data);
+    });
+  } else { // link already cached.
+    var obj = cache[url];
+    obj.hits++;
+    callback(null, obj.data);
+  }
+}
 
 /* GET users listing. */
 router.get('/thumbnail/:url', function(req, res) {
@@ -37,35 +100,15 @@ router.get('/thumbnail/:url', function(req, res) {
     return;
   }
 
-  if (!cache[url] || !cache[url].data) {
-    // cache link
-    cache[url] = {
-      url: url,
-      hits: 0,
-      created: new Date().getTime()
+  getImgData(url, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).end();
+      return;
     }
 
-    jthumb.getSiteThumbnail(url, function(err, data) {
-      if (err) {
-        console.log(err);
-        res.end(400);
-        return;
-      }
-
-      // cache it
-      var obj = cache[url];
-      obj.data = data;
-      obj.hits++;
-
-      count++;
-
-      res.end(data, 'binary');
-    })
-  } else { // link already cached.
-    var obj = cache[url];
-    obj.hits++;
-    res.end(obj.data, 'binary');
-  }
+    res.end(data, 'binary');
+  });
 });
 
 
@@ -73,16 +116,7 @@ var descRegex = /<meta\s+.*["']description["']\s+content=["']([^"']*)["']\s+\/?>
 //var titleRegex = /<meta\s+[property=]?[]?\s+content=["'](*)["']/;
 var titleRegex = /<title>\s*(.*)\s*<\s*\/title>/im;
 
-router.get('/info/:url', function(req, res) {
-  var url = req.params.url;
-
-  console.log("URL: " + url);
-
-  if (!url) {
-    res.end(404);
-    return;
-  }
-
+function getImgInfo(url, callback) {
   if (!(cache[url] && cache[url].desc && cache[url].title)) {
 
     var pu = parseuri(url);
@@ -92,14 +126,14 @@ router.get('/info/:url', function(req, res) {
       path: pu.relative || pu.patch
     };
 
-    http.request(opts, function(hresp) {
+    var req = http.request(opts, function(res) {
       var str = '';
 
-      hresp.on('data', function(chunk) {
+      res.on('data', function(chunk) {
         str += chunk;
       });
 
-      hresp.on('end', function() {
+      res.on('end', function() {
         // check for description
         var descArr = descRegex.exec(str);
         var titleArr = titleRegex.exec(str);
@@ -121,22 +155,47 @@ router.get('/info/:url', function(req, res) {
         console.log(cache[url].title);
 
         // send the info
-        res.send({
-          desc: cache[url].desc || "No description found.",
-          title: cache[url].title || "No Title found."
+        callback(null, {
+          desc: cache[url].desc || (pu.host + pu.path) || "No description found.",
+          title: cache[url].title || pu.host || "No Title found."
         });
-        res.end();
       })
-    }).end();
+    });
 
+    req.on('error', function(err) {
+      console.log('req problem: ' + err.message);
+    });
+
+    req.end();
   } else {
     // send the info
-    res.send({
+    callback(null, {
       desc: cache[url].desc,
       title: cache[url].title
     });
-    res.end();
   }
+}
+
+router.get('/info/:url', function(req, res) {
+  var url = req.params.url;
+
+  console.log("URL: " + url);
+
+  if (!url) {
+    res.end(404);
+    return;
+  }
+
+  getImgInfo(url, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).end();
+      return;
+    }
+
+    res.send(data);
+    res.end();
+  })
 });
 
 
