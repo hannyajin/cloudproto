67287d8d7b89808ae2c45c643479a13a8cf90313
Hannyajin <talmo.christian@gmail.com>
2014-10-13 03:43:36 +0300

    thumbnail module added

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', routes);
 app.use('/users', users);
 
